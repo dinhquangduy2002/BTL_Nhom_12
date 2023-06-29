30a5dfd9a062a748b14350c31ce645d97593e18e
tran hieu <iostrunghieu@gmail.com>
2023-06-29 14:46:31 +0700

    fix manifest

--- MyFoodApp/app/src/main/java/com/pnhue/myfoodapp/MainActivity.java
+++ MyFoodApp/app/src/main/java/com/pnhue/myfoodapp/MainActivity.java
@@ -23,6 +23,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.pnhue.myfoodapp.activities.WelcomeActivity;
 import com.pnhue.myfoodapp.databinding.ActivityMainBinding;
 import com.pnhue.myfoodapp.models.UserModel;
 
