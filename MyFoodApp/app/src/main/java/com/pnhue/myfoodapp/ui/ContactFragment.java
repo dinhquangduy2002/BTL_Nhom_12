fe86ef2dc1242d7079e2fc1b24b152453fd341f0
tran hieu <iostrunghieu@gmail.com>
2023-07-01 01:01:17 +0700

    fix

--- MyFoodApp/app/src/main/java/com/pnhue/myfoodapp/ui/ContactFragment.java
+++ MyFoodApp/app/src/main/java/com/pnhue/myfoodapp/ui/ContactFragment.java
@@ -38,104 +38,16 @@ import java.util.List;
 
 
 public class ContactFragment extends Fragment {
-    EditText Name, Phone, Email, Comment;
-    Button btnSend;
-    TextView imgMail, imgCall;
-    FirebaseFirestore db;
-    FirebaseAuth auth;
 
 
-//    public ContactFragment() {
-//        // Required empty public constructor
-//    }
+
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_contact, container, false);
-        Name = root.findViewById(R.id.contact_name);
-        Phone = root.findViewById(R.id.contact_phone);
-        Email = root.findViewById(R.id.contact_email);
-        Comment = root.findViewById(R.id.contact_comment);
-        btnSend = root.findViewById(R.id.contact_btn_send);
-        imgCall = root.findViewById(R.id.idCall);
-        imgMail = root.findViewById(R.id.idEmail);
-
-        auth = FirebaseAuth.getInstance();
-        db = FirebaseFirestore.getInstance();
-
-
-        //xu ly click
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String userEmail = Email.getText().toString();
-                String userName = Name.getText().toString();
-                String phone = Phone.getText().toString();
-                String text = Comment.getText().toString();
-                if (userEmail.length() == 0|| text.isEmpty()) {
-                    Toast.makeText(getActivity(), "Vui lòng nhập Email!!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (userName.length() == 0|| text.isEmpty()) {
-                    Toast.makeText(getActivity(), "Vui lòng nhập họ và tên!!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (text.length() == 0|| text.isEmpty()) {
-                    Toast.makeText(getActivity(), "Vui lòng nhập nội dung cần liên hệ!!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (phone.length() == 0|| text.isEmpty()) {
-                    Toast.makeText(getActivity(), "Vui lòng nhập số điện thoại!!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                final HashMap<String, Object> ContactMap = new HashMap<>();
-                ContactMap.put("name", Name.getText().toString());
-                ContactMap.put("email",Email.getText().toString());
-                ContactMap.put("phone", Phone.getText().toString());
-                ContactMap.put("comment", Comment.getText().toString());
-
-                db.collection("CurrentUser").document(auth.getCurrentUser().getUid())
-                        .collection("Contact").add(ContactMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-                            @Override
-                            public void onComplete(@NonNull Task<DocumentReference> task) {
-                                Toast.makeText(getContext(), "Gửi liên hệ thành công", Toast.LENGTH_SHORT).show();
-
-                            }
-                        });
-                Name.setText("");
-                Phone.setText("");
-                Email.setText("");
-                Comment.setText("");
-            }
-        });
-
-        imgCall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //khai bao intent an
-                Intent callintent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:19008523"));
-//                //yeu cau su dong y cua nguoi dung
-                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-
-                    ActivityCompat.requestPermissions((Activity) getContext(), new String[]{Manifest.permission.CALL_PHONE}, 1);
-                    return;
-                }
-                //v.getContext().startActivity(intent);
-                startActivity(callintent);
-            }
-        });
-        imgMail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_SENDTO);
-                intent.setData(Uri.parse("mailto:toongteam@gmail.com")); // only email apps should handle this
-                intent.putExtra(Intent.EXTRA_EMAIL, "");
-                intent.putExtra(Intent.EXTRA_SUBJECT, "");
-                startActivity(intent);
-            }
-        });
         return root;
     }
 
